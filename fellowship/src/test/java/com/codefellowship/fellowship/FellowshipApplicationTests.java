778717ce368b20a69adecb154c4aad273da36dea
Yajahira Velazquez <104327409+firecracker80@users.noreply.github.com>
2022-09-06 19:40:04 -0400

    Add files via upload

---
+++ fellowship/src/test/java/com/codefellowship/fellowship/FellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefellowship.fellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
